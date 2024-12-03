ac29e6fa975112104b0a1f38b0b0ae3c6cb5b395
Noman Shoukat <nomanaadma@gmail.com>
2024-12-03 05:32:38 -0800

    init

---
+++ config/config.php
@@ -0,0 +1,38 @@
+<?php
+/*This name will represent title in auto generated mail*/
+define('NAME', 'Dvago');
+/*Domain name like www.yourdomain.com*/
+define('URL', 'http://pharmapos.test/');
+
+
+/*Application Address*/
+define('DIR_ROUTE', 'index.php?route=');
+define('DIR', 'D:/laragon/www/pharmaPOS/');
+define('DIR_APP', DIR.'app/');
+define('DIR_BUILDER', DIR.'builder/');
+define('DIR_VIEW', DIR_APP.'views/');
+define('DIR_IMAGE', DIR.'public/images/');
+define('DIR_UPLOADS', DIR.'public/uploads/');
+define('DIR_STORAGE', DIR_BUILDER.'storage/');
+define('DIR_LIBRARY', DIR_BUILDER.'library/');
+define('DIR_VENDOR', DIR_BUILDER.'vendor/');
+define('DIR_LANGUAGE', DIR_APP.'language/');
+
+
+/** MySQL settings - You can get this info from your web host **/
+/*MySQL database host*/
+define('DB_HOSTNAME', 'localhost');
+/*MySQL database username*/
+define('DB_USERNAME', 'root');
+/*MySQL database password*/
+define('DB_PASSWORD', '');
+/*MySQL database Name*/
+define('DB_DATABASE', 'pharmaPOS');
+/*Table Prefix*/
+define('DB_PREFIX', 'pp_');
+
+
+define('AUTH_KEY', 'vfFqY19bxffbtHz10l9)DCQ97DD)U8%m*zSiJ?L8&1igSWD)kM3H)dPscl-Tvvc6');
+define('LOGGED_IN_SALT', 'Moq||2x(cG&n>Nn*GH2(YA5#LExDmmH01BHv2b#Q7cq18H31#sUJFoJ)#rXK8;eu');
+define('TOKEN', '4hIQ41wW4yj%r)q|(P1XZ}|aZqDxP#}ZL<?-lbP)*f#~M7}tZOhox%K-0Dmhn;4>');
+define('TOKEN_SALT', 'NyOQgpVwSrIfG6d4*MmY)SOVoNa)K}uD*C1KMc1xhu9Vuqm53Nx>t1JmJ0E5quc>');
