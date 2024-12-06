2902ada37a658d141d65a02a934f22a4524c3633
Noman Shoukat <nomanaadma@gmail.com>
2024-12-06 08:33:18 -0800

    generic query builder

--- app/http/models/Customer.php
+++ app/http/models/Customer.php
@@ -3,7 +3,10 @@
 /**
  * Customer.php
  */
-class Customer extends Model
+
+include_once('Search.php');
+
+class Customer extends Search
 {
 
 	public function getCustomer($id)
@@ -14,12 +17,6 @@ class Customer extends Model
 
 	public function filterCustomer($options)
 	{
-
-		$offset = $options['start'];
-		$length = $options['length'];
-		if($length == '-1') {
-			$length = 100000000;
-		}
 
 		$sqlQuery = "SELECT *, CONCAT(firstname, ' ', lastname) AS 'name' FROM `" . DB_PREFIX . "customers` WHERE 1=?";
 		
@@ -35,148 +32,16 @@ class Customer extends Model
 			$sqlQuery .= " OR created_date like '%".$search."%')";
 		}
 
-		if(isset($options['searchBuilder'])) {
+        $queries = $this->queryBuilder($options, $sqlQuery);
 
-			foreach ($options['searchBuilder']['criteria'] as $key => $criteria) {
-
-				if(!isset($criteria['condition'])) continue;
-				
-				$columnName = $criteria['origData'];
-				
-				if($columnName == 'name') {
-					$columnName = "CONCAT(firstname, ' ', lastname)";
-				}
-
-				$value = isset($criteria['value']) ? $criteria['value'] : '';
-
-				$conditionBuilder = $this->conditionBuilder($columnName, $criteria['condition'], $value, $criteria['type']);
-				$sqlQuery .= "$conditionBuilder";
-
-			}
-
-		}
-		
-		$filteredQuery = $this->database->query($sqlQuery, [1]);
-
-		if(isset($options['order'])) {
-			
-			$order = $options['order'][0];
-			$orderCol = $order['column'];
-			$orderDir = strtoupper($order['dir']);
-
-			$columnName = $options['columns'][$orderCol]['data'];
-
-			$sqlQuery .= " ORDER BY $columnName $orderDir";
-
-		}
-
-		$sqlQuery .= " LIMIT $length OFFSET $offset";
-
-		$dataQuery = $this->database->query($sqlQuery, [1]);
 
 		$countQuery = $this->database->query("SELECT COUNT(*) as total FROM `" . DB_PREFIX . "customers`");
 		
 		return [
-			'data' => $dataQuery->rows, 
-			'recordsFiltered' => $filteredQuery->num_rows,
+			'data' => $queries['dataQuery']->rows,
+			'recordsFiltered' => $queries['filteredQuery']->num_rows,
 			'total' => (int)$countQuery->row['total']
 		];
-	}
-
-	public function conditionBuilder($columnName, $criteriaCondition, $criteriaValue, $criteriaType) {
-		
-		$conditions = [
-			'=' => '=',
-			'!=' => '!=',
-			'starts' => 'like',
-			'!starts' => 'not like',
-			'contains' => 'like',
-			'!contains' => 'not like',
-			'ends' => 'like',
-			'!ends' => 'not like',
-			'<' => '<',
-			'<=' => '<=',
-			'>' => '>',
-			'>=' => '>=',
-			'null' => 'null',
-			'!null' => '!null',
-			'between' => 'between',
-			'!between' => 'not between'
-		];
-
-		if($criteriaType == 'date') {
-
-			if($criteriaCondition == '=') {
-				$criteriaCondition = 'contains';
-			}
-
-			if($criteriaCondition == '!=') {
-				$criteriaCondition = '!contains';
-			}
-
-		}
-
-		$condition = $conditions[$criteriaCondition];
-		$value = ($criteriaValue != '') ? $criteriaValue[0] : '';
-
-		if( $criteriaCondition == 'starts' || $criteriaCondition == '!starts' ) {
-			return " AND $columnName $condition '$value%'";
-		}
-
-		if( $criteriaCondition == 'ends' || $criteriaCondition == '!ends' ) {
-			return " AND $columnName $condition '%$value'";
-		}
-
-		if($criteriaCondition == 'contains' || $criteriaCondition == '!contains') {
-			return " AND $columnName $condition '%$value%'";
-		}
-
-		if($criteriaCondition == 'null') {
-			return " AND ($columnName = '' OR $columnName IS NULL)";
-		}
-
-		if($criteriaCondition == '!null') {
-			return " AND ($columnName != '' AND $columnName IS NOT NULL)";
-		}
-		
-		if((
-			$criteriaCondition == '<' || 
-			$criteriaCondition == '<=' ||
-			$criteriaCondition == '>' || 
-			$criteriaCondition == '>='
-			)
-			&& is_numeric($value)
-		) {
-			return " AND $columnName $condition $value";
-		}
-
-		if(
-			($criteriaCondition == 'between' || $criteriaCondition == '!between') && 
-			$value != '' && 
-			isset($criteriaValue[1]) && 
-			$criteriaValue[1] != ''
-		) {
-			$value2 = $criteriaValue[1];
-
-			if($criteriaType == 'date') {
-				$value = "'$value'";
-				$value2 = "'$value2'";	
-			}
-
-			return " AND ($columnName $condition $value AND $value2)";
-		}
-
-		if(
-			$criteriaCondition == '=' ||
-			$criteriaCondition == '!=' ||
-			$criteriaCondition == '<' ||
-			$criteriaCondition == '<=' ||
-			$criteriaCondition == '>' ||
-			$criteriaCondition == '>=' 
-		) {
-			return " AND $columnName $condition '$value'";
-		}
-
 	}
 
 	public function getBills($data)
---
+++ app/http/models/Search.php
@@ -0,0 +1,155 @@
+﻿<?php
+
+class Search extends Model
+{
+    protected function conditionBuilder($columnName, $criteriaCondition, $criteriaValue, $criteriaType) {
+
+        $conditions = [
+            '=' => '=',
+            '!=' => '!=',
+            'starts' => 'like',
+            '!starts' => 'not like',
+            'contains' => 'like',
+            '!contains' => 'not like',
+            'ends' => 'like',
+            '!ends' => 'not like',
+            '<' => '<',
+            '<=' => '<=',
+            '>' => '>',
+            '>=' => '>=',
+            'null' => 'null',
+            '!null' => '!null',
+            'between' => 'between',
+            '!between' => 'not between'
+        ];
+
+        if($criteriaType == 'date') {
+
+            if($criteriaCondition == '=') {
+                $criteriaCondition = 'contains';
+            }
+
+            if($criteriaCondition == '!=') {
+                $criteriaCondition = '!contains';
+            }
+
+        }
+
+        $condition = $conditions[$criteriaCondition];
+        $value = ($criteriaValue != '') ? $criteriaValue[0] : '';
+
+        if( $criteriaCondition == 'starts' || $criteriaCondition == '!starts' ) {
+            return " AND $columnName $condition '$value%'";
+        }
+
+        if( $criteriaCondition == 'ends' || $criteriaCondition == '!ends' ) {
+            return " AND $columnName $condition '%$value'";
+        }
+
+        if($criteriaCondition == 'contains' || $criteriaCondition == '!contains') {
+            return " AND $columnName $condition '%$value%'";
+        }
+
+        if($criteriaCondition == 'null') {
+            return " AND ($columnName = '' OR $columnName IS NULL)";
+        }
+
+        if($criteriaCondition == '!null') {
+            return " AND ($columnName != '' AND $columnName IS NOT NULL)";
+        }
+
+        if((
+                $criteriaCondition == '<' ||
+                $criteriaCondition == '<=' ||
+                $criteriaCondition == '>' ||
+                $criteriaCondition == '>='
+            )
+            && is_numeric($value)
+        ) {
+            return " AND $columnName $condition $value";
+        }
+
+        if(
+            ($criteriaCondition == 'between' || $criteriaCondition == '!between') &&
+            $value != '' &&
+            isset($criteriaValue[1]) &&
+            $criteriaValue[1] != ''
+        ) {
+            $value2 = $criteriaValue[1];
+
+            if($criteriaType == 'date') {
+                $value = "'$value'";
+                $value2 = "'$value2'";
+            }
+
+            return " AND ($columnName $condition $value AND $value2)";
+        }
+
+        if(
+            $criteriaCondition == '=' ||
+            $criteriaCondition == '!=' ||
+            $criteriaCondition == '<' ||
+            $criteriaCondition == '<=' ||
+            $criteriaCondition == '>' ||
+            $criteriaCondition == '>='
+        ) {
+            return " AND $columnName $condition '$value'";
+        }
+
+    }
+
+    protected function queryBuilder($options, $sqlQuery) {
+
+        if(isset($options['searchBuilder'])) {
+
+            foreach ($options['searchBuilder']['criteria'] as $key => $criteria) {
+
+                if(!isset($criteria['condition'])) continue;
+
+                $columnName = $criteria['origData'];
+
+                if($columnName == 'name') {
+                    $columnName = "CONCAT(firstname, ' ', lastname)";
+                }
+
+                $value = isset($criteria['value']) ? $criteria['value'] : '';
+
+                $conditionBuilder = $this->conditionBuilder($columnName, $criteria['condition'], $value, $criteria['type']);
+                $sqlQuery .= "$conditionBuilder";
+
+            }
+
+        }
+
+        $filteredQuery = $this->database->query($sqlQuery, [1]);
+
+        if(isset($options['order'])) {
+
+            $order = $options['order'][0];
+            $orderCol = $order['column'];
+            $orderDir = strtoupper($order['dir']);
+
+            $columnName = $options['columns'][$orderCol]['data'];
+
+            $sqlQuery .= " ORDER BY $columnName $orderDir";
+
+        }
+
+        $offset = $options['start'];
+        $length = $options['length'];
+        if($length == '-1') {
+            $length = 100000000;
+        }
+
+        $sqlQuery .= " LIMIT $length OFFSET $offset";
+
+        $dataQuery = $this->database->query($sqlQuery, [1]);
+
+        return [
+            "filteredQuery" => $filteredQuery,
+            "dataQuery" => $dataQuery
+        ];
+
+    }
+
+}
